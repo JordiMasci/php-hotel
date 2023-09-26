1db3178838b24ac260d9747e24889d88c87302ff
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-26 16:30:50 +0200

    feat: ciclo per ogni valore

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +54,18 @@
 <body>
     <div class="container mt-5">
         <h1>Lista Hotel</h1>
+        <hr>
+        <ul>
+            <?php foreach ($hotels as $hotel): ?>
+            <li>
+                <h2><?php echo $hotel['name'] ?></h2>
+                <p>Descrizione: <?php echo $hotel['description'] ?></p>
+                <p>parcheggio: <?php echo $hotel['parking'] ?></p>
+                <p>voto: <?php echo $hotel['vote'] ?></p>
+                <p>distanza dal centro: <?php echo $hotel['distance_to_center'] ?> km</p>
+            </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
 </body>
 </html>
