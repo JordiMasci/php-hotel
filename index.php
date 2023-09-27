93574ef6be0b7186a9827692d2c6a38f117f8c37
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-09-27 09:42:36 +0200

    fix

--- index.php
+++ index.php
@@ -40,6 +40,7 @@
 
     ];
 
+
 ?>
 
 <!DOCTYPE html>
@@ -51,9 +52,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 </head>
+
+
 <body>
     <div class="container mt-5">
         <h1 class="mb-3">Lista Hotel</h1> 
+
+        <form action="" method="GET">
+    <label for="filtro-parcheggio">Filtra per parcheggio:</label>
+    <select name="filtro-parcheggio" id="filtro-parcheggio">
+        <option value="">Tutti</option>
+        <option value="si">Con parcheggio</option>
+        <option value="no">Senza parcheggio</option>
+    </select>
+    <input type="submit" value="Filtra">
+</form>
         
         <table class="table table-bordered ">
   <thead>
@@ -73,8 +86,6 @@
       <td><?php echo $hotel['name'] ?></td>
       <td>Descrizione: <?php echo $hotel['description'] ?></td>
       <td>parcheggio: <?php echo ($hotel['parking'] ? 'Presente' : 'Assente');?></td>
-
-
       <td>voto: <?php echo $hotel['vote'] ?></td>
       <td>distanza dal centro: <?php echo $hotel['distance_to_center'] ?> km</td>
     </tr>
